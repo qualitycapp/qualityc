e05e688d99ebe86e187bca8860c0ca323124c3a5
Olcay Bayram <obayram@suitsupply.com>
2020-08-15 21:45:59 +0200

    Rollback env variables

--- config.js
+++ config.js
@@ -1,8 +1,6 @@
 // config.js
 
 module.exports = {
-    clientId: process.env.production ?
-        "bc94ea3c80919ac83867" : "816cddbfe65b5b44beaf",
-    authenticationServer: process.env.production ?
-        "https://qualityc-hodor.herokuapp.com" : "https://gatokeepero.herokuapp.com"
+    clientId: "bc94ea3c80919ac83867",
+    authenticationServer: "https://qualityc-hodor.herokuapp.com"
 }
